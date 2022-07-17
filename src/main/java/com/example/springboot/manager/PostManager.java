c7292ed45fea02b85ef602bede2cc55b98858d89
alexskyteam <injenermven@mail.ru>
2022-07-17 12:06:48 +0300

    OnoToMany added

--- src/main/java/com/example/springboot/manager/PostManager.java
+++ src/main/java/com/example/springboot/manager/PostManager.java
@@ -1,31 +1,41 @@
 package com.example.springboot.manager;
 
+import com.example.springboot.dto.AuthorDTO;
 import com.example.springboot.dto.GeoDTO;
 import com.example.springboot.dto.PostRequestDTO;
 import com.example.springboot.dto.PostResponseDTO;
 import com.example.springboot.entity.GeoEmbeddable;
 import com.example.springboot.entity.PostEntity;
+import com.example.springboot.entity.UserEntity;
 import com.example.springboot.exception.PostNotFoundException;
 import com.example.springboot.repository.PostRepository;
+import com.example.springboot.repository.UserRepository;
 import com.example.springboot.security.Authentication;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Component
+@Transactional
 @RequiredArgsConstructor
 public class PostManager {
     private final PostRepository postRepository; // мне через DI подставят нужный интерфейс
+    private final UserRepository userRepository;
     private final Function<PostEntity, PostResponseDTO> postEntityToPostResponseDTO = postEntity -> new PostResponseDTO(
             postEntity.getId(),
+            new AuthorDTO(postEntity.getAuthor().getId(), postEntity.getAuthor().getLogin()),
+            //Optional.of(postEntity.getAuthor()).map(o -> new AuthorDTO(o.getId(), o.getLogin())).get(),
             postEntity.getContent(),
             postEntity.getTags(),
             // postEntity.getGeo() != null ? new Geo(postEntity.getGeo().getLat(), postEntity.getGeo().getLng()),
-            Optional.of(postEntity.getGeo()).map(o -> new GeoDTO(o.getLat(), o.getLng())).orElse(null)
+            Optional.ofNullable(postEntity.getGeo())
+                    .map(o -> new GeoDTO(o.getLat(), o.getLng()))
+                    .orElse(null)
     );
 
     public List<PostResponseDTO> getAll(final Authentication authentication) {
@@ -44,11 +54,15 @@ public class PostManager {
     }
 
     public PostResponseDTO create(final Authentication authentication, final PostRequestDTO requestDTO) {
+        final UserEntity userEntity = userRepository.getReferenceById(authentication.getId());
         final PostEntity postEntity = new PostEntity(
                 0,
+                userEntity,
                 requestDTO.getContent(),
                 requestDTO.getTags(),
-                Optional.of(requestDTO.getGeo()).map(o -> new GeoEmbeddable(o.getLat(), o.getLng())).orElse(null)
+                Optional.ofNullable(requestDTO.getGeo())
+                        .map(o -> new GeoEmbeddable(o.getLat(), o.getLng()))
+                        .orElse(null)
         );
         final PostEntity savedEntity = postRepository.save(postEntity);
         return postEntityToPostResponseDTO.apply(savedEntity);
@@ -62,7 +76,9 @@ public class PostManager {
         postEntity.setContent(requestDTO.getContent());
         postEntity.setTags(requestDTO.getTags());
         postEntity.setGeo(
-                Optional.of(requestDTO.getGeo()).map(o -> new GeoEmbeddable(o.getLat(), o.getLng())).orElse(null)
+                Optional.ofNullable(requestDTO.getGeo())
+                        .map(o -> new GeoEmbeddable(o.getLat(), o.getLng()))
+                        .orElse(null)
         );
         return postEntityToPostResponseDTO.apply(postEntity);
     }
